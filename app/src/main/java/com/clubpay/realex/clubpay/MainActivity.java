4b33a8dd2efd61c877fe780c806e7f197d67d366
Arturoo <synowiec.artur85@gmail.com>
2016-03-12 08:56:44 +0000

    Basic functionality v2

--- app/src/main/java/com/clubpay/realex/clubpay/MainActivity.java
+++ app/src/main/java/com/clubpay/realex/clubpay/MainActivity.java
@@ -5,20 +5,28 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Region;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.clubpay.realex.clubpay.Model.ServerModel;
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //new HttpRequestTask().execute();
+
         beaconManager = new BeaconManager(getApplicationContext());
 
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
@@ -61,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
                         "Current security wait time is 15 minutes, "
                                 + "and it's a 5 minute walk from security to the gate. "
                                 + "Looks like you've got plenty of time!");
+                new HttpRequestTask().execute();
+
+
             }
 
             @Override
@@ -109,4 +122,43 @@ public class MainActivity extends AppCompatActivity {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, notification);
     }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, ResponseEntity> {
+
+        private  String AMAZONE_URL_TMP = "http://clubpay.vrwuqqpad3.eu-west-1.elasticbeanstalk.com";
+        @Override
+        protected ResponseEntity doInBackground(Void... params) {
+            try {
+
+                RestTemplate restTemplate = new RestTemplate();
+                //restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                ServerModel model = new ServerModel();
+                TelephonyManager tMgr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                String imeiNo = tMgr.getDeviceId();
+                model.setImeiNo(imeiNo);
+                String url = AMAZONE_URL_TMP.concat("/payAction?phoneId=").concat(imeiNo);
+                ResponseEntity ent = restTemplate.postForEntity(url, model, Boolean.class);
+                //Map<String,String> queryParameter = new HashMap<String,String>();
+                //queryParameter.put("phoneId",imeiNo);
+                //ResponseEntity ent = restTemplate.postForEntity("http://localhost:8080/payAction", model, Boolean.class, queryParameter);
+
+                return ent;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ResponseEntity greeting) {
+           // TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+           // TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+           // greetingIdText.setText(greeting.getId());
+           // greetingContentText.setText(greeting.getContent());
+        }
+
+    }
 }
+
+
---
+++ app/src/main/java/com/clubpay/realex/clubpay/Model/ServerModel.java
@@ -0,0 +1,17 @@
+package com.clubpay.realex.clubpay.Model;
+
+/**
+ * Created by artursynowiec on 12/03/16.
+ */
+public class ServerModel {
+
+    private String imeiNo;
+
+    public String getImeiNo() {
+        return imeiNo;
+    }
+
+    public void setImeiNo(String imeiNo) {
+        this.imeiNo = imeiNo;
+    }
+}
